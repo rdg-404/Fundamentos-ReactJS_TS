a74f828e3fe4376f99f1018033f254f2f878c434
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2022-12-07 00:09:44 -0300

    arrumando erros do avatar.tsx (props da interface opcionais)

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,10 +1,24 @@
 import styles from './Avatar.module.css'
 
+
+
+interface AvatarProps {
+    hasBorder?: boolean;
+    src: string;
+    alt?: string;
+}
+
+
 //desinstruturação js 
 // voce pode passar valores para as props ao desinstruturar
 
-export function Avatar({hasBorder = true, src}) {
+export function Avatar({hasBorder = true, src, alt}: AvatarProps) {
     return (
-        <img className={hasBorder ? styles.avatarWithBorder : styles.avatar} src={src}  />
+        <img 
+            className={hasBorder ? styles.avatarWithBorder : styles.avatar}
+            src={src} 
+            alt={alt} 
+        
+        />
     )
 } 
